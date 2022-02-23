9bdd6864806a8f3792c30405b70ee3bad121f6d2
Godlove <ntangakalegodlove@gmail.com>
2022-02-23 22:19:20 +0100

    added some some comments

--- blog.php
+++ blog.php
@@ -1,19 +1,20 @@
-
+<!--important links required on the page-->
 <?php include 'scripts/dbconn.php';?>
 <?php include 'includes/links.php';?>
 <?php include 'scripts/articleupload.php'?>
 <body>
-	<!--header-->
+	<!--header bar-->
 <?php include 'includes/header.php';?>
-
-<!--posts-->
+<!--header bar-->
+	
+<!--All posts container-->
 <main>
 	
 	<div class="tit">
 		<h2>News and Aticles :</h2>
 	</div>
 	
-<!--blog posts-->
+<!--blog posts wrapper-->
 <div class="blog-wrapper">
 <?php include 'parts/article.php';?>
 </div>
@@ -24,16 +25,19 @@
 						<img src="icons/1x/outline_article_black_24dp.png" alt="create post" id="createpost">
 						<p class="create-text">Create Article</p>
 					</div>
-
-					<!--create a post form-->
+						<!--/create a post button-->
+					
+					<!-- form to create a form linked from includes folder-->
 				<?php include 'includes/articlemodal.php';?>
 				</div>
 
 
 <!--navigation bar-->
 	<?php include 'includes/navbar.php';?>
-<!--loading more posts-->
+	
+<!--loading/spinner animation to load more posts-->
 	<?php include 'includes/spinner.php';?>
+	<!--splash screen-->
 		<?php include 'loader.php';?>
 </main>
 
