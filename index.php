e98508f7a5f88b07148e9cf8e101e7ec4120585b
Godlove <ntangakalegodlove@gmail.com>
2022-02-23 22:23:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-
+<!--important links required on the site-->
 <?php include 'scripts/dbconn.php';?>
 <?php include 'scripts/comment.php';?>
 <?php include 'scripts/postupload.php';?>
 <?php include 'includes/links.php';?>
 <?php include 'scripts/follow.php';
-
+// when the user tries to access this page without first login/signup they are redirected to login page//
 if(!isset($_SESSION['username'])){
 header("location: login.php");
 exit();
@@ -17,15 +17,16 @@ exit();
 <!--posts-->
 <main>
 
-<!--post contianer-->
+<!--post contianer to load all posts-->
 <?php include 'parts/allposts.php';?>
 
-	<!--create a post form-->
+	<!--create a post form/maodal to create a post-->
 	<?php include 'includes/postmodal.php';?>
-<!--navigation bar-->
+<!-- bottom navigation bar-->
 	<?php include 'includes/navbar.php';?>
 <!--loading more posts-->
 	<?php include 'includes/spinner.php';?>
+	<!--splash screen-->
 	<?php include 'loader.php';?>
 </main>
 
